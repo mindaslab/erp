5e17fa38d4256089c7bcae7a3afb9de7f98e0863
Karthikeyan A K <77minds@gmail.com>
2014-06-02 19:22:09 +0530

    designed companies and books page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -28,7 +28,7 @@ class CompaniesController < ApplicationController
 
     respond_to do |format|
       if @company.save
-        format.html { redirect_to @company, notice: 'Company was successfully created.' }
+        format.html { redirect_to companies_url, notice: 'Company was successfully created.' }
         format.json { render :show, status: :created, location: @company }
       else
         format.html { render :new }
