28874e2d7746287a70b30d827695a9e4346045c7
Karthikeyan A K <77minds@gmail.com>
2014-09-25 11:29:03 +0530

    added loan to balance

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -13,7 +13,8 @@ class RecordsController < ApplicationController
     revenue = @records.revenue.sum(:amount)
     expense = @records.expense.sum(:amount)
     @income = revenue - expense
-    @balance = @income + @records.capital.sum(:amount) - @records.draw.sum(:amount)
+    @balance = @income + @records.capital.sum(:amount) - @records.draw.sum(:amount) +
+    @records.loan.sum(:amount)
   end
 
   # GET /records/1
