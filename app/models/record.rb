c63a1c93890744ee2940ecde8acfea3af4021d3a
Karthikeyan A K <77minds@gmail.com>
2014-09-27 11:16:06 +0530

    set up width for description column

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,8 +1,14 @@
 class Record < ActiveRecord::Base
   enum status: [:revenue, :expense, :pending_revenue, :pending_expense,
-  	:capital, :draw, :loan_taken]
+    :capital, :draw, :loan_taken]
   belongs_to :book
+  before_create :assign_sno
+
   SearchableColumns = [:description]
-  
   include Searchable
+
+  private
+  def assign_sno
+    self.sno = self.book.records.maximum(:sno) + 1
+  end
 end
