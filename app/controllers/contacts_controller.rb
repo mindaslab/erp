467455a5d666e208ef9d7fa73e16763f483f4787
Karthikeyan A K <77minds@gmail.com>
2014-10-06 08:11:35 +0530

    fixed company contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -67,7 +67,7 @@ class ContactsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_contact
-      @contact = @company.contact.find(params[:id])
+      @contact = @company.contacts.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
