9e3f7465309764c50ff137146263658991f373aa
Karthikeyan A K <karthikeyan.ak@zanec.com>
2014-12-06 22:37:56 +0530

    beautified forms

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -49,7 +49,7 @@ class CompaniesController < ApplicationController
   def update
     respond_to do |format|
       if @company.update(company_params)
-        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.html { redirect_to companies_url, notice: 'Company was successfully updated.' }
         format.json { render :show, status: :ok, location: @company }
       else
         format.html { render :edit }
